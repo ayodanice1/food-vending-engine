d3c7213003b733767c603e63866ed66a42830203
ayodele <ayodanice1@gmail.com>
2020-07-11 13:48:43 +0100

    Modify apiviews and serializer on app/menu

--- menu/api/apiviews.py
+++ menu/api/apiviews.py
@@ -2,61 +2,50 @@ from rest_framework import generics, permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
 
 from ..models import Day, Menu
 from .serializers import MenuSerializer
 
 
-class MenuList(APIView):
+class MenuList(generics.ListCreateAPIView):
+    serializer_class = MenuSerializer
     
-    def get(self, request):
-        if request.user.is_vendor:
-            menus = Menu.objects.filter(vendor=request.user)
+    def get_queryset(self):
+        if self.request.user.is_vendor:
+            queryset = Menu.objects.filter(vendor=self.request.user)
         else:
-            menus = Menu.objects.all()
-        data = MenuSerializer(menus, many=True).data
-        return Response(data)
+            queryset = Menu.objects.all()
+        return queryset
     
     def post(self, request):
-        if request.user.is_vendor:
-            menu = Menu.objects.create(
-                name = request.data['name'],
-                description = request.data['description'],
-                quantity = request.data['quantity'],
-                price = request.data['price'],
-                vendor = request.user,
-            )
-            scheduled_days = (request.data['scheduled_days']).strip().split(' ')
-            for scheduled_day in scheduled_days:
-                menu.scheduled_days.add(Day.objects.get(name=scheduled_day))
-            data = MenuSerializer(menu).data
-            return Response(data)
-        return Response({ 'detail': 'You do not have the permission to perform this action' })
+        if request.user.id != int(request.data['vendor']):
+            raise PermissionDenied
+        serializer = MenuSerializer(data=request.data)
+        if serializer.is_valid():
+            menu = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class MenuDetail(APIView):
+class MenuDetail(generics.RetrieveUpdateAPIView):
+    serializer_class = MenuSerializer
     
     def get(self, request, pk):
         menu = get_object_or_404(Menu, pk=pk)
         if menu.vendor == request.user or not request.user.is_vendor:
             data = MenuSerializer(menu).data
             return Response(data)
-        return Response({ 'detail': 'You do not have the permission to perform this action' })
+        raise PermissionDenied
 
     def put(self, request, pk):
         menu = get_object_or_404(Menu, pk=pk)
         if menu.vendor != request.user:
-            return Response({ 'detail': 'You do not have the permission to perform this action' })
-        for k, v in request.data.items():
-            if k == 'name':
-                menu.name = v
-            elif k == 'description':
-                menu.description = v
-            elif k == 'quantity':
-                menu.quantity = int(v)
-            elif k == 'price':
-                menu.price = float(v)
-            elif k == 'scheduled_days':
-                return Response({ 'detail': 'Working on updating scheduled days'})
-            menu.save()
-        return Response({ 'detail': 'Menu info updated successfully' })
-    
+            raise PermissionDenied
+        serializer = MenuSerializer(menu, data=request.data)
+        if serializer.is_valid():
+            menu = serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
--- menu/api/serializers.py
+++ menu/api/serializers.py
@@ -7,4 +7,20 @@ class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = ( 'id', 'name', 'description', 'price', 'quantity', 'scheduled_days', 'vendor', )
-    
+    '''
+    def create(self, validated_data):
+        if self.request.user.is_vendor:
+            menu = Menu.objects.create(
+                name = validated_data['name'],
+                description = request.data['description'],
+                quantity = request.data['quantity'],
+                price = request.data['price'],
+                vendor = request.user,
+            )
+            scheduled_days = (request.data['scheduled_days']).strip().split(' ')
+            for scheduled_day in scheduled_days:
+                menu.scheduled_days.add(Day.objects.get(name=scheduled_day))
+            data = MenuSerializer(menu).data
+            return Response({ 'detail': 'getting there' })
+        raise PermissionError
+    '''
